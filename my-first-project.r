14bafbe3fda16b90862067f74572a03120ab2746
Giorgio Nava <gionava2000@gmail.com>
2025-01-04 15:54:37 +0000

    Kaggle Notebook | my-first-project | Version 1

---
+++ my-first-project.r
@@ -0,0 +1,92 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "9d974f27",
+   "metadata": {
+    "papermill": {
+     "duration": 0.001739,
+     "end_time": "2025-01-04T15:54:28.383175",
+     "exception": false,
+     "start_time": "2025-01-04T15:54:28.381436",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "source": [
+    "#Hello Kaggle"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "5b354bc0",
+   "metadata": {
+    "execution": {
+     "iopub.execute_input": "2025-01-04T15:54:28.389530Z",
+     "iopub.status.busy": "2025-01-04T15:54:28.387418Z",
+     "iopub.status.idle": "2025-01-04T15:54:28.554581Z",
+     "shell.execute_reply": "2025-01-04T15:54:28.552403Z"
+    },
+    "papermill": {
+     "duration": 0.172708,
+     "end_time": "2025-01-04T15:54:28.557134",
+     "exception": false,
+     "start_time": "2025-01-04T15:54:28.384426",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[1] \"Hello Kaggle\"\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello Kaggle\")"
+   ]
+  }
+ ],
+ "metadata": {
+  "kaggle": {
+   "accelerator": "none",
+   "dataSources": [],
+   "dockerImageVersionId": 30749,
+   "isGpuEnabled": false,
+   "isInternetEnabled": false,
+   "language": "r",
+   "sourceType": "notebook"
+  },
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.4.0"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 3.464233,
+   "end_time": "2025-01-04T15:54:28.679022",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2025-01-04T15:54:25.214789",
+   "version": "2.6.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
